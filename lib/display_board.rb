657022d26f385f92e3f184e2b2c70b5e8d1b97d1
John Casabar <johncasabar@gmail.com>
2018-10-11 03:01:57 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,7 +4,8 @@ row_divider = -----------
 
 def display_board(state)
   puts " #{state[0]} | #{state[1]} | #{state[2]} "
-
+  puts row_divider
   puts " #{state[3]} | #{state[4]} | #{state[5]} "
-
+  puts row_divider
   puts " #{state[6]} | #{state[7]} | #{state[8]} "
+end
